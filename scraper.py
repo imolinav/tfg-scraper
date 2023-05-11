54a5ed34f3f434d6511f8dbccda5b5803a4b390c
imolinav <iamovaz@gmail.com>
2023-05-11 20:19:57 +0200

    feat: connection to database working

--- scraper.py
+++ scraper.py
@@ -1,1 +1,12 @@
-from scrapy.item import Item, Field
+import psycopg2
+
+hostname = 'localhost'
+username = 'imolinav'
+password = 'imolinav'
+database = 'postgres'
+
+connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+cur = connection.cursor()
+
+cur.execute("""INSERT INTO cities (name, postal_code) values (%s, %s)""", ('Valencia', str(46016)))
+connection.commit()
